40ca9bbd97c1afd578a55abb2f5c95e968b04587
Sebastian López <sebasgal608@gmail.com>
2026-02-16 15:11:24 -0500

    Is multiple of another

---
+++ LogicConcepts2025-1/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,15 @@
+﻿using Shared;
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if(b % a == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es múltiplo de {b}");
+    }
+} while (true);
