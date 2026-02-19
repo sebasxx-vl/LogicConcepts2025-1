6caec55f36946505cb87747a4e9ccf78fc0a00c2
Sebastian López <sebasgal608@gmail.com>
2026-02-19 14:39:58 -0500

    Fibonacci modified

---
+++ LogicConcepts2025-1/FibonacciModified/Program.cs
@@ -0,0 +1,29 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    var n = ConsoleExtension.GetInt("Cuántos términos quiere (mínimo 3): ");
+    double a = 0;
+    double b = 1;
+    double c = 2;
+
+    Console.Write($"{a,20:N0}\t{b,20:N0}\t{c,20:N0}");
+
+    for (int i = 2; i < n; i++)
+    {
+        double d = a + b + c;
+        Console.Write($"{d,20:N0}\t");
+        a = b;
+        b = c;
+        c = d;
+    }
+    Console.WriteLine();
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
