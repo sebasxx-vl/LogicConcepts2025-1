806c6ac9f9b961026f8275f9938c44d16eb5d831
Sebastian López <sebasgal608@gmail.com>
2026-02-18 22:08:03 -0500

    Transportation calculation

--- LogicConcepts2025-1/Shared/Shared/ConsoleExtension.cs
+++ LogicConcepts2025-1/Shared/Shared/ConsoleExtension.cs
@@ -1,4 +1,19 @@
 ﻿namespace Shared
+/*
+using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+
+do
+{
+    answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
+*/
 {
     public class ConsoleExtension
     {
